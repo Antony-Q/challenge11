cbc0e33a85c984861f29ec2aab28e0cf94720677
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-05-24 17:17:26 -0500

    push to heroku...   >.>

--- server.js
+++ server.js
@@ -9,10 +9,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// Use apiRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
