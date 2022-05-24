4a680e102d52d236e0381a13d97f453d98406bbe
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-05-24 17:02:38 -0500

    pushing to heroku

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const { webpage } = require('./Develop/public/assets/js/index');
 const express = require('express');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
-app.get('./Develop/public/notes.html', (req, res) => {
-    res.json(webpage);
-  });
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
