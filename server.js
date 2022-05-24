986ca67910ed87d871631807160aba91d71d2469
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-05-24 17:26:10 -0500

    fixed server router connection issue hopefully

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./Develop/routes/apiRoutes');
+const htmlRoutes = require('./Develop/routes/htmlRoutes');
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use('./apiRoutes', apiRoutes);
-app.use('./htmlRoutes', htmlRoutes);
+app.use('./Develop/routes/apiRoutes', apiRoutes);
+app.use('./Develop/routes/htmlRoutes', htmlRoutes);
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
