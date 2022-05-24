a8502e1f86d94ed1c317cbbd01722294edb00bb6
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-05-24 16:33:05 -0500

    edited index htmlroutes

--- Develop/routes/htmlRoutes/index.js
+++ Develop/routes/htmlRoutes/index.js
@@ -1,7 +1,16 @@
-const express = require('express');
-const router = express.Router();
+const path = require('path');
+const router = require('express').Router();
 
-router.use(require('./htmlRoutes'));
-router.use(require('./apiRoutes.js'));
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
+});
+
+router.get('/notes/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/notes.html"));
+})
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
+});
 
 module.exports = router;
