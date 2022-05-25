6431a3a8b6a7112728e3fd1ce36386a340a4233c
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-05-25 17:15:45 -0500

    fhdkfd

--- app/server.js
+++ app/server.js
@@ -6,7 +6,7 @@ const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/apiRoutes", apiRoutes);
